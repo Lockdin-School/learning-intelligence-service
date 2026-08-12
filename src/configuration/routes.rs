ad59885d685470626d1311c9a41cd4ce80c63ad0
Somelele Quse <quse@mergenti.com>
2026-08-12 14:36:17 +0200

    Initial commit

---
+++ src/configuration/routes.rs
@@ -0,0 +1,9 @@
+use actix_web::web;
+
+pub fn configure(cfg: &mut web::ServiceConfig) {
+    log::info!("Configuring routes...");
+    cfg.service(
+        // ------------- configure routes ------------
+        web::scope("/api/v1").service(web::scope("/auth")),
+    );
+}
